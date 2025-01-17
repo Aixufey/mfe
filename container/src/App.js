0aa497abe185a8171a21af84e111475e5c02a75f
Aixufey <jack_11728@msn.com>
2025-01-17 22:43:17 +0100

    trigger action

--- container/src/App.js
+++ container/src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default function App() {
-  return <div>
-    <h1>Hi there!</h1>
-    <hr />
-    <MarketingApp />
-  </div>;
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 }
