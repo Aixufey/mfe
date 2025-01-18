591328675347f5d45293055078aa27829f6e3b20
Aixufey <jack_11728@msn.com>
2025-01-18 21:21:56 +0100

    Implement routing synchronization between container and Marketing app using custom history management

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,11 +1,26 @@
 // Importing remotes as mount functions - no coupling - generic integration only
 import { mount } from 'marketing/MarketingIndex';
 import React, { useEffect, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
+
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: (location) => {
+        const { pathname: nextPathname } = location;
+
+        const { pathname: currentPath } = history.location;
+        if (currentPath !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+    // Container listen to change to child's location - if child changes, update container
+    history.listen(onParentNavigate)
+
   }, []);
 
   return <div ref={ref} />;
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,17 +1,39 @@
+import { createBrowserHistory, createMemoryHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 // Mount fn to start the app
-const mount = (e) => {
-  ReactDOM.render(<App />, e);
+const mount = (e, { onNavigate, defaultHistory }) => {
+  // subapp will use memory history to avoid browser history when navigating
+  // Marketing loads up, receives onNavigate from container, creates a memory history and listens to it.
+  // When Marketing changes routes, returns
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    // Receives location from container, pushes to memory history
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, e);
+
+  return {
+    // Returns a callback that will sync the container with the subapp
+    onParentNavigate(location) {
+      const { pathname: nextPathname } = location;
+      const { pathname: currentPathname } = history.location;
+      if (currentPathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If dev mode mount dev
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#dev-root-marketing');
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
